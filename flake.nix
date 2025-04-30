8337685b63901e74729c7a12d29eeded6b0236cc
Yehor Borkov <yehor.borkov@gmail.com>
2025-04-30 11:14:40 +0000

    feat: add pulp-riscv-gnu-toolchain package

--- flake.nix
+++ flake.nix
@@ -16,7 +16,31 @@
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
-      packages = forEachSystem (system: { });
+      packages = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config = { };
+            overlays = [ self.overlays.default ];
+          };
+        in
+        {
+          pulp-riscv-gnu-toolchain =
+            let
+              base = inputs.pulpissimo.packages.${system}.pulp-riscv-gnu-toolchain.override {
+                stdenv = pkgs.gcc9CcacheStdenv;
+              };
+            in
+            base.overrideAttrs {
+              configureFlags = [
+                "--with-arch=rv64gcv"
+                "--with-cmodel=medlow"
+                "--enable-multilib"
+              ];
+            };
+        }
+      );
 
       devShells = forEachSystem (
         system:
@@ -24,11 +48,23 @@
           pkgs = import nixpkgs {
             inherit system;
             config = { };
+            overlays = [ self.overlays.default ];
           };
         in
         {
           default = { };
         }
       );
+
+      overlays.default = final: prev: {
+        ccacheWrapper = prev.ccacheWrapper.override {
+          extraConfig = ''
+            export CCACHE_COMPRESS=1
+            export CCACHE_DIR=/nix/var/cache/ccache
+            export CCACHE_UMASK=007
+          '';
+        };
+        gcc9CcacheStdenv = final.ccacheStdenv.override { stdenv = prev.gcc9Stdenv; };
+      };
     };
 }
