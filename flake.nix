099b348bdf6d6e7c19281b740cc9478addd348ed
Yehor Borkov <yehor.borkov@gmail.com>
2025-04-30 11:18:53 +0000

    refactor: extract pkgs to separate attribute

--- flake.nix
+++ flake.nix
@@ -16,31 +16,30 @@
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
-      packages = forEachSystem (
+      pkgs = forEachSystem (
         system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            config = { };
-            overlays = [ self.overlays.default ];
-          };
-        in
-        {
-          pulp-riscv-gnu-toolchain =
-            let
-              base = inputs.pulpissimo.packages.${system}.pulp-riscv-gnu-toolchain.override {
-                stdenv = pkgs.gcc9CcacheStdenv;
-              };
-            in
-            base.overrideAttrs {
-              configureFlags = [
-                "--with-arch=rv64gcv"
-                "--with-cmodel=medlow"
-                "--enable-multilib"
-              ];
-            };
+        import nixpkgs {
+          inherit system;
+          config = { };
+          overlays = [ self.overlays.default ];
         }
       );
+
+      packages = forEachSystem (system: {
+        pulp-riscv-gnu-toolchain =
+          let
+            base = inputs.pulpissimo.packages.${system}.pulp-riscv-gnu-toolchain.override {
+              stdenv = self.pkgs.${system}.gcc9CcacheStdenv;
+            };
+          in
+          base.overrideAttrs {
+            configureFlags = [
+              "--with-arch=rv64gcv"
+              "--with-cmodel=medlow"
+              "--enable-multilib"
+            ];
+          };
+      });
 
       devShells = forEachSystem (
         system:
